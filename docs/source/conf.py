f264714ff753da814b9c9e28ed68beae0d555b1c
Nick Murphy <namurphy@cfa.harvard.edu>
2024-07-02 20:08:39 -0400

    pre-commit run --all-files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'hack'
-copyright = '2024, PlasmaPy Community'
-author = 'PlasmaPy Community'
-release = '0.1.0'
+project = "hack"
+copyright = "2024, PlasmaPy Community"
+author = "PlasmaPy Community"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
