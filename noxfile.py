2d573c5ee85783ab9ee57ece86fb56afe5ab6e94
Nick Murphy <namurphy@cfa.harvard.edu>
2024-07-02 20:02:57 -0400

    Add Nox session for building documentation

--- noxfile.py
+++ noxfile.py
@@ -9,3 +9,16 @@ def tests(session):
     """Run tests with pytest."""
     session.install(".")
     session.run("pytest")
+
+
+@nox.session
+def docs(session):
+    """Build documentation with Sphinx."""
+    session.install(".")
+    session.run(
+        "sphinx-build",
+        "docs/source",
+        "docs/build/html",
+        "--builder",
+        "html",
+    )
